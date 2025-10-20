836acd5adeeb6577f4bd617f856e4102965a7945
Mariana <myronchuk.mariana@gmail.com>
2025-10-20 10:46:03 +0200

    task 1

--- goit-pycore-hw-03.py
+++ goit-pycore-hw-03.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-date = input("Введіть дату у форматі 'YYYY-MM-DD':, ")
+
 def get_days_from_today(date: str) -> int:
     try:
         input_date = datetime.strptime(date, "%Y-%m-%d").date()
@@ -8,4 +8,7 @@ def get_days_from_today(date: str) -> int:
         return delta.days
     except ValueError:
         raise ValueError("Неправильний формат дати. Використовуйте формат 'YYYY-MM-DD'.")
-    print (delta.days)
+
+date = input("Введіть дату у форматі 'YYYY-MM-DD': ")
+
+print(get_days_from_today(date)) 
